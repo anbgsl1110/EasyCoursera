756c2c257bec21bdaec63b96ff9f5be9e8b2612b
jason <jasonwangex@gmail.com>
2017-03-02 08:48:44 +0800

    change：修改 healthcheck

--- src/main/java/com/jasonwangex/easyCoursera/web/TestController.java
+++ src/main/java/com/jasonwangex/easyCoursera/web/TestController.java
@@ -1,17 +1,28 @@
 package com.jasonwangex.easyCoursera.web;
 
+import com.jasonwangex.easyCoursera.dao.UserDao;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * Created by wangjz
  * on 17/2/24.
  */
-@RequestMapping("/check/health")
+@RequestMapping("/healthcheck")
 @RestController
 public class TestController {
+    @Resource
+    private UserDao userDao;
+
     @RequestMapping()
     public String test(){
+        try {
+            userDao.getById(1);
+        } catch (Exception e) {
+            return "error";
+        }
         return "success";
     }
 }
