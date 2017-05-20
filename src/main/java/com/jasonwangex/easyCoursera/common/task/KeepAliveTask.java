532d80513ae0744a647924718225e72b9d92adf3
jason <jasonwangex@gmail.com>
2017-05-20 14:00:25 +0800

    add：新增 jdbc keepAlive

---
+++ src/main/java/com/jasonwangex/easyCoursera/common/task/KeepAliveTask.java
@@ -0,0 +1,28 @@
+package com.jasonwangex.easyCoursera.common.task;
+
+import com.jasonwangex.easyCoursera.account.dao.EcUserDao;
+import com.jasonwangex.easyCoursera.account.domain.EcUser;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+
+/**
+ * Created by wangjz
+ * on 17/5/20.
+ */
+@Component
+public class KeepAliveTask {
+    @Resource
+    private EcUserDao ecUserDao;
+
+    @Scheduled(fixedDelay = 10000L)
+    public void keepAlive(){
+        try {
+            EcUser user = ecUserDao.getOne(Collections.emptyList(), null);
+        } catch (Exception ignore) {
+
+        }
+    }
+}
